63626b9dd2edc9b44c2e2b6d30393fd4ef31a924
Olawoyin Joseph Folasakin <olawoyinjoseph05@gmail.com>
2026-06-01 10:35:46 +0100

    Add PageNotFound component and integrate routing in App for 404 handling

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Card from './components/Card'
 import Card2 from './components/Card2'
 import QualitiesPage from './components/QualitiesPage'
 import ImagesCard from './components/ImagesCard'
+import { Route, Routes } from 'react-router-dom'
+import PageNotFound from './components/PageNotFound'
 
 const App = () => {
   return (
@@ -17,6 +19,10 @@ const App = () => {
       <QualitiesPage/>
       <Card2/>
       <ImagesCard/>
+
+      <Routes>
+         <Route path="*" element={<PageNotFound/>}/>
+      </Routes>
     </>
   )
 }
